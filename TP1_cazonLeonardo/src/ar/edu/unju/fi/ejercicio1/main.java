823d11a71bbad9b1d019f404968b58309fc16e03
Leonardo Cazon <leonardonahuelcazongomez1234@gmail.com>
2024-04-04 16:15:33 -0300

    ejercicio 1 - Cazon Leonardo

---
+++ TP1_cazonLeonardo/src/ar/edu/unju/fi/ejercicio1/main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//1)promedio
+		
+		int numero1 = 5;
+		int numero2 = 5;
+		int numero3 = 5;
+		int numero4 = 5;
+		int numero5 = 5;
+		
+		double promedio =  (double) (numero1+numero2+numero3+numero4+numero5)/5 ;
+		
+		System.out.println("el preomedio es:" +promedio);
+	}
+
+}
