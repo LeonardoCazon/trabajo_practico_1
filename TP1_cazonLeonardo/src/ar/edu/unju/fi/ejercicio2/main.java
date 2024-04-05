31087a8a49a91c58b914d2d8a8de222eb302cdf1
CazonLeonardo <leofa@DESKTOP-KV5FD2M>
2024-04-05 20:00:22 -0300

    ejercicio 2

---
+++ TP1_cazonLeonardo/src/ar/edu/unju/fi/ejercicio2/main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String pais = "argentina";
+		int edad= 18;
+		float altEdif = 1440;
+		String telefono = "388-0000 001";
+		double resultado = Math.cos(0.5);
+		
+		System.out.println("Datos Almacenados:"+ "\nPais:" + pais  + "\nEdad:" + edad  + 
+							"\nAltura Del Edificio(Metros):" + altEdif + "\nTelefono:" + telefono + 
+								"\nresultado de cos de 0.5: " + resultado);
+	}
+
+}
