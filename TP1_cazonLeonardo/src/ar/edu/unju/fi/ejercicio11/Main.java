df8c753434f357a78c4c74f29175081f560b2723
Leonardo Cazon <leofa@DESKTOP-KV5FD2M>
2024-04-18 14:46:26 -0300

    ejercicio 11

---
+++ TP1_cazonLeonardo/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i , j, num = 80;
+		
+		for(i = 2, j = 40; j > 0; i++, j--) {
+			System.out.print(num + " ");
+			num += (j-i-1);
+		}
+	}
+}
